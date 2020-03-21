7c9312a3a82f97f5f9883d3733c81fe3ed65e5b1
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-03-21 18:00:24 +0900

    Added the number of reviews, the list of reviews, and the close function of the app
    レビュー数の表示、レビューの一覧表示、アプリの終了機能を追加

--- Tech.scala
+++ Tech.scala
@@ -1,13 +1,9 @@
 import scala.io.StdIn
+import scala.util.control.Breaks._
 
-// def menu = {
-  println("レビュー数：0")
-  println("[0]レビューを書く")
-  println("[1]レビューを読む")
-  println("[2]アプリを終了する")
-  var input = StdIn.readInt()
+var post: Array[String] = Array.empty
 
-if( input == 0){
+def post_review = {
   val line = "-----------------------------"
   println(line)
   println("著者を入力してください")
@@ -19,16 +15,37 @@ if( input == 0){
   println("価格を入力してください")
   val price:String = StdIn.readLine()
   println("価格："+price+"\n")
+  post = post :+ name
 
   println(line)
   println("著者："+name)
   println("タイトル："+tittle)
   println("価格："+price)
   println(line)
-}else if( input == 1){
-  println("レビュー一覧：")
-}else if(input == 2){
+}
 
+def read_reviews = {
+  println("レビュー一覧")
+  for(i <- post){
+    println(i)
+  }
+}
+
+while(true){
+println("レビュー数：" +post.length)
+println("[0]レビューを書く")
+println("[1]レビューを読む")
+println("[2]アプリを終了する")
+var input = StdIn.readInt()
+
+if( input == 0){
+  post_review
+}else if( input == 1){
+  read_reviews
+}else if(input == 2){
+  println("アプリを終了")
+  break
 }else{
   println("入力された値は無効な値です")
+}
 }
