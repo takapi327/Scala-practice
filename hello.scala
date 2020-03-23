9836bfd0152263c4255e34387c5c762a6a647530
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-03-23 20:59:35 +0900

    Practice Scala
    Scalaコードの復習

--- hello.scala
+++ hello.scala
@@ -1,13 +1,5 @@
 // 文字の表示
 // println("Hello," + args(0) + "!")
-
-
-var i = 0
-while(i < args.length){
-  println(args(i))
-  i += 1
-}
-
 // class Main {
 
 //     // 出力
@@ -20,7 +12,7 @@ while(i < args.length){
 //     printf("Hello, %s!\n", "World")
 // }
 
-
+// while分の使用法
 // var i = 0
 // while (i < args.length) {
 //   if (i != 0)
@@ -30,10 +22,21 @@ while(i < args.length){
 // }
 // println()
 
-// args.foreach((arg: String) => prinln(arg))
+// foreach分の使用法
+// args.foreach(println)
 
+// for分の使用法
 // for (arg <- args)
 //   println(arg)
+
+// 配列の使用法
+// val num = Array("zero", "one", "two")
+// val num = new Array[String](3)
+// num(0) = "Hello"
+// num(1) = "world"
+// num(2) = "!"
+// for(i <- 0 to 2)
+//   println(num(i))
 
 // import scala.io.Source
 
