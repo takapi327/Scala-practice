e8aaededef798b6932c7070d1122a1343606c82a
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-04-30 23:07:47 +0900

    Create 09.scala file

---
+++ N-school/09.scala
@@ -0,0 +1,30 @@
+object Match extends App {
+  /*
+  def printRandomChars(): Unit = {
+    for (i <- 1 to 1000) {
+      val chars: Seq[Char] = new scala.util.Random(new java.security.SecureRandom()).alphanumeric.take(5).toList
+      val result = chars match {
+        case Seq(a, b, c, d, _) => Seq(a, b, c, d, a)
+      }
+      println(result)
+    }
+  }
+  println(printRandomChars)
+
+  def last(seq: Seq[Int]): Int = {
+    seq match {
+      case Seq(x) => return x
+      case x::xs => last(xs)
+    }
+  }
+  println(last(Seq(1,2,3)))
+  */
+
+  def reverse(seq: Seq[Int]): Seq[Int] = {
+    seq match {
+      case Seq(x) => Seq(x)
+      case x::xs => reverse(xs) :+ x
+    }
+  }
+  println(reverse(Seq(1, 2, 3)))
+}
